e7e30dea36695735fd9ea51ca0281300eb592370
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-10-24 15:32:55 +0530

    Update Home component layout and add image assets

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import { useRef, useState } from "react";
 import { FaChevronRight } from "react-icons/fa";
+import { ALL_IMAGES } from "../utils/images";
 
 function Home() {
+  const [scrollPostion, setScrollPostion] = useState(0);
+
+  const containerRef = useRef();
   return (
     <div className="w-full">
-      <div className="">
+      <div className="first">
         <div className="relative w-full">
           <div className="netflix-background-img">
             <img
@@ -43,12 +48,37 @@ function Home() {
                 placeholder="Email address"
                 className="border py-4 pl-5 pr-40 rounded-sm"
               />
-              <button className="flex items-center justify-center gap-3 bg-red-600 hover:bg-red-700 rounded-md px-10 cursor-pointer">
-                <span className="text-xl font-bold">Get Started</span>
+              <button className="flex items-center justify-center gap-3 bg-red-600 hover:bg-red-700 rounded-md px-6 cursor-pointer">
+                <span className="text-[23px] font-bold tracking-wider">
+                  Get Started
+                </span>
                 <FaChevronRight size={18} />
               </button>
             </div>
           </div>
+          <div className="absolute w-full">
+            <div className="w-full border-5 border-red-600 "></div>
+          </div>
+        </div>
+      </div>
+      <div className="second w-full h-[50vh] px-13 mt-5">
+        <div className="container flex justify-center items-center gap-2">
+          <button className="border w-[92px] h-[35px] rounded-lg">
+            Left button
+          </button>
+          <div ref={containerRef} className="w-[900px]">
+            <div className="content-box flex gap-5">
+              {ALL_IMAGES.map((item) => (
+                <div className="card bg-neutral-500 h-[20vh] w-[10vw]">
+                  <p>{item.alt}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <button className="border w-[102px] h-[35px] rounded-lg">
+            Right button
+          </button>
         </div>
       </div>
     </div>
---
+++ Frontend/src/utils/images.js
@@ -0,0 +1,12 @@
+export const ALL_IMAGES = [
+  { id: "01", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "02", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "03", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "04", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "05", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "06", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "07", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "08", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "09", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+  { id: "10", src: "/They_call_him_OG.webp", alt: "They call him OG" },
+];
