622f63da7afb20ead30e06708f3fd7b0d83609ef
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-10-27 12:26:00 +0530

    Fix import statement for icons and clean up FAQSection layout for better readability

--- Frontend/src/pages/home/FAQSection.jsx
+++ Frontend/src/pages/home/FAQSection.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { data } from "../../utils/FAQSectionData";
-import { FaPlus } from "react-icons/fa";
-import { FaMinus } from "react-icons/fa6";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 function FAQSection() {
   const [seletecd, setSelected] = useState(null);
@@ -13,23 +12,26 @@ function FAQSection() {
   return (
     <div className="w-full py-5 px-27 flex flex-col gap-3">
       <h2 className="text-2xl font-bold">Frequently Asked Questions</h2>
-      <div className="relative flex flex-col items-center gap-5">
+      <div className="relative flex flex-col items-center gap-5 w-full">
         {data.map((dataItem) => (
           <div
+            key={dataItem.id}
             onClick={() => handleSingleSelaticon(dataItem.id)}
-            className="item w-full"
+            className="item w-full cursor-pointer"
           >
             <div className="flex justify-between items-center p-6 w-full  bg-amber-700 text-white tracking-wide">
               <h2 className="text-xl">{dataItem.question}</h2>
-              <span>
-                {seletecd === dataItem.id ? <FaMinus /> : <FaPlus />}
-              </span>
+              <span>{seletecd === dataItem.id ? <FaMinus /> : <FaPlus />}</span>
             </div>
-            {seletecd === dataItem.id ? (
-              <div className="bg-amber-600 text-white text-[20px] p-6 mt-1">
-                {dataItem.answer}
-              </div>
-            ) : null}
+            <div
+              className={`overflow-hidden bg-amber-600 text-white text-[20px] px-6 mt-1 transition-all duration-300 ease-in-out ${
+                seletecd === dataItem.id
+                  ? "max-h-40 py-4 opacity-100"
+                  : "max-h-0 opacity-0"
+              }`}
+            >
+              {dataItem.answer}
+            </div>
           </div>
         ))}
       </div>
