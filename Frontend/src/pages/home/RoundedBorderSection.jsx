147660c906bf0dbf4fda229b02e1fc3d87764013
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-10-28 15:35:40 +0530

    Succefully create the Rounded border section and that complete

--- Frontend/src/pages/home/RoundedBorderSection.jsx
+++ Frontend/src/pages/home/RoundedBorderSection.jsx
@@ -4,11 +4,26 @@ function RoundedBorderSection() {
   return (
     <div className="relative w-full h-[30vh] bg-black overflow-hidden flex items-center justify-center">
       <div
-        className="absolute w-[130%] h-[120px] left-[-15%] bottom-0 border-4 border-b-0 border-red-600 rounded-tl-[50%_100%] rounded-tr-[50%_100%]"
+        className="absolute w-[130%] h-[120px] left-[-15%] bottom-0 rounded-tl-[50%_100%] rounded-tr-[50%_100%] p-1"
         style={{
-            background:"radial-gradient(50% 500% at 50% -420%,rgba(64,97,231,0.4) 80%,rgba(0,0,0,0.1) 100%),black"
+          background: `linear-gradient(
+          to right,
+          rgba(33, 13, 22, 1) 16%,
+          rgba(184, 40, 105, 1),
+          rgba(229, 9, 20, 1),
+          rgba(184, 40, 105, 1),
+          rgba(33, 13, 22, 1) 84%
+          )`,
+          clipPath: "inset(0 0 8% 0 round 50% 50% 0 0)",
         }}
-      > 
+      >
+        <div
+          className="w-full h-full rounded-tl-[50%_100%] rounded-tr-[50%_100%]"
+          style={{
+            background:
+              "radial-gradient(50% 500% at 50% -420%,rgba(64,97,231,0.4) 80%,rgba(0,0,0,0.1) 100%),black",
+          }}
+        ></div>
       </div>
     </div>
   );
